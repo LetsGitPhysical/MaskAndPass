b5fcb5d64720d201483165396468ca27dceb73cd
podasin <podasin@gmail.com>
2022-09-08 12:38:47 -0400

    removed commented clutter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,11 @@
 
 
-// -----------------------  Now you can see how something complex is spatially in 4 separate sections to think clearer -------
-
 var Uppers = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var Downers = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var Numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var Specials = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
 
-
-  function getPasswordOptions() {//--------------------  Much More Visually Intuitive  -----------------------------------
+  function getPasswordOptions() { 
     var length = parseInt(prompt('How many characters would you like your password to contain?'));
       if (Number.isNaN(length)) {
         alert('Password length must be provided as a number');
@@ -22,7 +19,7 @@ var Specials = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}'
         alert('Password length must less than 129 characters');
         return null;
       }
-      // has X 4
+      // has X 4   instead, maybe make as check box, add listener and generate after...
       var hasSpecials = confirm('Click OK to confirm including special characters.');
       var hasNumeros = confirm('Click OK to confirm including numeric characters.' );
       var hasDowners = confirm('Click OK to confirm including lowercase characters.');
@@ -30,36 +27,20 @@ var Specials = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}'
 
     if (hasSpecials === false && hasNumeros === false && hasDowners === false && hasUppers === false) {
       alert('Must select at least one character type');
-      return null;   //--------cancel nation
+      return null;   
     }
-    //--------------------   some hash action..   ----------------------------
-    var passwordOptions = {
-      length: length,
-      hasSpecials: hasSpecials,
-      hasNumeros: hasNumeros,
-      hasDowners: hasDowners,
-      hasUppers: hasUppers
-    };
+    var passwordOptions = {length: length, hasSpecials: hasSpecials, hasNumeros: hasNumeros, hasDowners: hasDowners, hasUppers: hasUppers };
     return passwordOptions;
-  } 
- //-------------------------------------------------------   getPasswordOptions   Cuts off here  -------------------------------------                            
-  
- //---------------------------------  Array of Randos.. -----------------------
+  }                         
   function getRandom(arr) { 
     var randIndex = Math.floor(Math.random() * arr.length); 
     var randElement = arr[randIndex];
     return randElement;
   }
-  
-  function generatePassword() {//-------------------------  generatePassword  ------------------------------------------------------------
+  function generatePassword() {
     var options = getPasswordOptions();
-    var result = [];
-    var maybeChrs = [];
-    var mustChars = [];
-
-    if (!options) return null;  // if not in the options.. 
-  
-    //--------------------------   Neat little trick i did  ------------------- --------------------------------------------
+    var [result,maybeChrs,mustChars] = [[],[],[]];
+    if (!options) return null;   
     var hasArr = [options.hasSpecials,options.hasNumeros,options.hasDowners,options.hasUppers];
     var randArr = [Specials,Numeros,Downers,Uppers];
     for(i = 0; i< randArr.length; i++){
@@ -68,29 +49,20 @@ var Specials = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}'
         mustChars.push(getRandom(randArr[i]));
       }
     }
-    //-------------------------------------------------------------------------- --------------------------------------------
-
-
     for (var i = 0; i < options.length; i++) {
       var possibleCharacter = getRandom(maybeChrs);
-  
       result.push(possibleCharacter);
     }
-  
     for (var i = 0; i < mustChars.length; i++) {
       result[i] = mustChars[i];
     }
-
     return result.join('');
-  }//-------------------------------------------------------  generatePassword  ------------------------------------------------------------
-
+  } 
 var generateBtn = document.querySelector('#generate');
-  
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
   passwordText.value = password;
 }  
-
 generateBtn.addEventListener('click', writePassword);
   
